a832373be6976af4cd04fcd10a99c61f4331fabb
Erkin Isoyev <grandboy99.ei@gmail.com>
2022-06-29 18:47:38 +0500

    add: news model

--- news/models.py
+++ news/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    name = models.CharField("Yangilik nomi", max_length=150)
+    text = models.TextField("Yangilik matni")
+
+    # more info
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Yangilik'
+        verbose_name_plural = 'Yangiliklar'
+
