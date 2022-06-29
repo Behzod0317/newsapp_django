6f08d0fb2d8ef560e968faf0d17e3b8ac8967df1
Erkin Isoyev <grandboy99.ei@gmail.com>
2022-06-29 20:11:35 +0500

    add: relational models

--- news/models.py
+++ news/models.py
@@ -1,9 +1,27 @@
+from unicodedata import category
 from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField("Yangilik bo'limi", max_length=200)
+
+    # more info
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Bo\'lim'
+        verbose_name_plural = 'Bo\'limlar'
+
 
 
 class News(models.Model):
     name = models.CharField("Yangilik nomi", max_length=150)
     text = models.TextField("Yangilik matni")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True, related_name="news")
 
     # more info
     created_at = models.DateTimeField(auto_now_add=True)
